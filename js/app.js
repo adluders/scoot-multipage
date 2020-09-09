30e2b0cbf130c8409cf0fbf6937747ea36a782cf
Adler Luders <adlerluders@gmail.com>
2020-09-09 19:52:56 -0400

    worked on mobile nav toggle on home page

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const faqTogglers = [...document.querySelectorAll(".faq__toggler")];
 const faqItems = [...document.querySelectorAll(".faq__body")];
+const mobileNavToggler = document.querySelector(".mobile-nav__toggler");
+const mobileNavDetails = document.querySelector(".mobile-nav__details");
+const overlay = document.querySelector(".mobile-nav__overlay");
+const body = document.querySelector("body");
 
 function toggleQuestion() {
   let btnClicked = faqTogglers.indexOf(this);
@@ -9,6 +13,31 @@ function toggleQuestion() {
   this.classList.toggle("is-rotated");
 }
 
+// set nav state
+let isNavOpen = false;
+
+const toggleNav = () => {
+  if (!isNavOpen) {
+    mobileNavToggler.innerHTML = `<img src="assets/icons/close.svg" alt="nav close icon" />`;
+    mobileNavDetails.style.display = "flex";
+    overlay.style.display = "block";
+    document.body.style.overflow = "hidden";
+  } else {
+    mobileNavToggler.innerHTML = `<img src="assets/icons/hamburger.svg" alt="nav open hamburger" />`;
+    mobileNavDetails.style.display = "none";
+    overlay.style.display = "none";
+    document.body.style.overflow = "scroll";
+  }
+
+  isNavOpen = !isNavOpen;
+};
+
+console.log(mobileNavDetails);
+
+// Event Listeners
+
 faqTogglers.forEach((toggle) =>
   toggle.addEventListener("click", toggleQuestion)
 );
+
+mobileNavToggler.addEventListener("click", toggleNav);
